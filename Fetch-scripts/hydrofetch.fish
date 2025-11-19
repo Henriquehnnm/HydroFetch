c935a1827f21146fdd50534e322f1f4bc4f8ac2d
Henriquehnnm <henriquesardinha987@gmail.com>
2025-11-19 07:36:03 -0300

    Update to 2.4.6 Beta 2

--- Fetch-scripts/hydrofetch.fish
+++ Fetch-scripts/hydrofetch.fish
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env fish
 
 # Versão
@@ -164,16 +165,10 @@ end
 # Exibir informações dentro de uma única caixa
 echo -e "$MAGENTA╭──────────────────────────────────────────╮$NC"
 printf "$MAGENTA│$WHITE $ICON_USER $MAGENTA│$WHITE User:   %-22s $MAGENTA     │$NC\n" "$USER"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_HOST $MAGENTA│$WHITE Host:   %-22s $MAGENTA     │$NC\n" "$HOST"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_OS $MAGENTA│$WHITE OS:     %-22s $MAGENTA     │$NC\n" "$OS"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_KERNEL $MAGENTA│$WHITE Kernel: %-22s $MAGENTA     │$NC\n" "$KERNEL"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_DE $MAGENTA│$WHITE DE:     %-22s $MAGENTA     │$NC\n" "$DE"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_RAM $MAGENTA│$WHITE RAM:    %-22s $MAGENTA     │$NC\n" "$RAM"
-echo -e "$MAGENTA│    $MAGENTA│$WHITE                                $MAGENTA     │$NC"
 printf "$MAGENTA│$WHITE $ICON_COLORS$MAGENTA │$WHITE Colors: $RED $NC  $GREEN $NC  $YELLOW $NC  $BLUE $NC  $MAGENTA $NC  $CYAN $NC  $WHITE $NC  $MAGENTA│$NC\n"
 echo -e "$MAGENTA╰──────────────────────────────────────────╯$NC"
