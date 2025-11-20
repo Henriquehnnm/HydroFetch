9d3141841dfa2e64dcfa81f4f79b6730e10806d0
Henriquehnnm <henriquesardinha987@gmail.com>
2025-11-20 09:37:53 -0300

    Update to 2.4.7 Beta 1

--- Fetch-scripts/hydrofetch.fish
+++ Fetch-scripts/hydrofetch.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 # Versão
-set VERSION "2.4.6 Fish Edition"
+set VERSION "2.4.7 Fish Edition"
 
 # Cores
 set RED '\033[1;31m'
@@ -16,10 +16,10 @@ set NC '\033[0m'
 # Informações do sistema
 set USER (whoami)
 set HOST $hostname
-set OS $(grep -E '^NAME=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
-set KERNEL $(uname -r)
-set DE (string replace -r '^$' N/A $XDG_CURRENT_DESKTOP)
-set RAM $(free -h --si | awk 'NR==2 {print $3 " / " $2}')
+set OS (grep -E '^NAME=' /etc/os-release | cut -d'=' -f2 | tr -d '"')
+set KERNEL (uname -r)
+set DE (string replace -r '^$' N/A (set -q XDG_CURRENT_DESKTOP; and echo $XDG_CURRENT_DESKTOP; or echo N/A))
+set RAM (free -h --si | awk 'NR==2 {print $3 " / " $2}')
 
 # Mostrar ajuda
 if test "$argv[1]" = "-h"
@@ -53,16 +53,16 @@ if not command -v figlet &>/dev/null
     echo -e "$YELLOW Dependencias não encontradas. Instalando..."
 
     if command -v apt &>/dev/null
-        sudo apt-get update && sudo apt-get install -y figlet
+        sudo apt-get update && sudo apt-get install -y figlet jq
     else if command -v dnf &>/dev/null
-        sudo dnf install -y figlet
+        sudo dnf install -y figlet jq
     else if command -v pacman &>/dev/null
-        sudo pacman -Sy --noconfirm figlet inetutils
+        sudo pacman -Sy --noconfirm figlet inetutils jq
     else if command -v zypper &>/dev/null
         sudo zypper refresh
-        sudo zypper --non-interactive install figlet
+        sudo zypper --non-interactive install figlet jq
     else if command -v apk &>/dev/null
-        sudo apk add figlet
+        sudo apk add figlet jq
     else
        echo -e "$RED Gerenciador de pacotes não suportado! Instale o Figlet e o Inetutils manualmente."
     end
@@ -116,10 +116,10 @@ if test "$argv[1]" = "-a"
       echo ""
 
       # Pega a interface de rede padrão
-      set interface (ip route | grep default | awk '{print $5}')
+      set interface (ip route | grep default | awk '{print $5}'; or echo N/A)
 
       # Pega o endereço IP associado à interface de rede
-      set ipaddr (ip -o -4 addr show "$interface" | awk '{print $4}' | cut -d/ -f1)
+      set ipaddr (ip -o -4 addr show $interface | awk '{print $4}' | cut -d/ -f1; or echo N/A)
 
       echo "IP: $(string replace -r '^$' N/A $ipaddr)"
       echo "Interface de rede: $(string replace -r '^$' N/A $interface)"
@@ -129,11 +129,18 @@ if test "$argv[1]" = "-a"
 end
 
 # Criar diretório ~/.hydrofetch
+# Dirs
 set HYDROFETCH_DIR "$HOME/.hydrofetch"
-set FONT_PATH "$HYDROFETCH_DIR/Custom.flf"
-mkdir -p "$HYDROFETCH_DIR"
+set PLUGINS_DIR "$HYDROFETCH_DIR/Plugins"
 
-# Nerd Font Icons (Certifique-se de ter uma fonte Nerd Font instalada!)
+# Path's
+set LOGOS_PATH "$PLUGINS_DIR/Logos.json"
+set FONT_PATH "$HYDROFETCH_DIR/Custom.flf"
+
+# Mkdir
+mkdir -p "$HYDROFETCH_DIR"
+mkdir -p "$PLUGINS_DIR"
+
 set ICON_USER " "
 set ICON_HOST "󰖟 "
 set ICON_OS "󰌽 "
@@ -142,14 +149,36 @@ set ICON_DE "󰍹 "
 set ICON_RAM " "
 set ICON_COLORS " "
 
-if test -f $FONT_PATH
-    echo -e "$CYAN"
-    figlet -f "$FONT_PATH" "$OS"
-    echo -e "$NC"
+if test -f $LOGOS_PATH
+
+    set LOGO (jq -r --arg os_name "$OS" '.[$os_name] // [] | .[]' "$LOGOS_PATH")
+
+    if test -n "$LOGO"
+        for line in $LOGO
+            echo -e "$CYAN$line$NC"
+        end
+    else
+        if test -f $FONT_PATH
+            echo -e "$CYAN"
+            figlet -f "$FONT_PATH" "$OS"
+            echo -e "$NC"
+        else
+            echo -e "$CYAN"
+            figlet "$OS"
+            echo -e "$NC"
+        end
+    end
+
 else
-    echo -e "$CYAN"
-    figlet "$OS"
-    echo -e "$NC"
+    if test -f $FONT_PATH
+        echo -e "$CYAN"
+        figlet -f "$FONT_PATH" "$OS"
+        echo -e "$NC"
+    else
+        echo -e "$CYAN"
+        figlet "$OS"
+        echo -e "$NC"
+    end
 end
 
 # Min mode
